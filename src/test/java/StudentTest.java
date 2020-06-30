050984278b7b47cd9fe9929242b4dfd53b6f7958
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-30 14:41:00 -0500

    created StudentTest class

---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StudentTest {
+
+//    @Before
+//    public void setUp() {
+//
+//    }
+
+    @Test
+    public void hasConstructor() {
+        Student student = new Student();
+
+    }
+}
