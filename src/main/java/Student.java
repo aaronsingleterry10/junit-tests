0ba3e55c7fe2926d65b1b286e9e80d3230efaaa0
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-30 13:51:02 -0500

    created Student class and added properties

---
+++ src/main/java/Student.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Student {
+//    id should be a long number used to represent a "unique user" in our application.
+    public long id;
+
+//    name is a String that holds the name of the student.
+    public String name;
+
+//    grades is an ArrayList that contains a list of Integer numbers.
+    public ArrayList<Integer> grades;
+
+}
